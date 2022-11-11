5cee303556ac077a9dcb5eaffeda5d16bdee8432
Marcus <hymanmarcus95@gmail.com>
2022-11-11 18:07:50 -0500

    build NavBar component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+function NavBar() {
+    return (
+        <header className="App-header">
+        <nav>
+            <Link className="App-link" to="/">Marcus Hyman</Link>
+            <Link className="App-link" to="/about">About Me</Link>
+            <Link className="App-link" to="/contact">Contact Me</Link>
+            <Link className="App-link" to="/projects">Projects</Link>
+        </nav>
+        </header>
+    )
+}
+
+export default NavBar;
